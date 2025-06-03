d181e0b0051d873c4b093e8ca641e75d8deda753
Geethareedy <geethareddydulla@gmail.com>
2025-06-03 16:25:32 +0530

    Add files via upload

---
+++ Main9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main9
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String a="geetha reddy";
+	   System.out.println(a);
+	    }
+}
+
