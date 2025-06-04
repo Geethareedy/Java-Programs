4f5fd2139db1fbc6f80432c8ad8f683ff053d8a5
Geethareedy <geethareddydulla@gmail.com>
2025-06-04 15:15:31 +0530

    Add files via upload

---
+++ Main15.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+public class Main15
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(1);
+	    s.push(2);
+	    s.push(3);
+		System.out.print(s);
+	}
+}
