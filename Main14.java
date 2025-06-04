7c7ac84f96b527fd0d93b6ca5edd3fbdf9c91212
Geethareedy <geethareddydulla@gmail.com>
2025-06-04 15:15:07 +0530

    Add files via upload

---
+++ Main14.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class Main14
+{
+	public static void main(String[] args) {
+	    Stack s=new Stack<>();
+	    s.push(1);
+	    s.push(2);
+	    s.push(3);
+		System.out.print(s.peek());
+		System.out.print(s.pop());
+	}
+}
