5b95944cf5d544bfd4f712f4a67b2eaf9afbaeb6
Geethareedy <geethareddydulla@gmail.com>
2025-06-13 16:19:57 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.Math;
+public class Positive
+{
+	public static void main(String[] args) {
+	   Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    if(a<0)
+	    {
+		System.out.println("postive");
+	}
+	else
+	{
+	    System.out.println("negative");
+	}
+}
+}
---
+++ Power.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.Math;
+public class Power
+{
+	public static void main(String[] args) {
+	   Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    double b=Math.pow(a,3);
+		System.out.println(b);
+	}
+}
